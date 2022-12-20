82989bb8e22c4c10945f814d069a90bcfde14650
Olga <starshova1309@gmail.com>
2022-12-20 12:20:07 +0300

    add component =>

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// no jsx
-// const title = React.createElement('h1', { id: 'title' }, 'Hello react')
-
-// with jsx
-// const title = <h1 id="title">Yo</h1>
-
-// const content = (
-//     <div>
-//         <ul>
-//             <li>Item 1</li>
-//             <li>Item 2</li>
-//         </ul>
-//         <p>hello </p>
-//     </div>
-// )
-
-function App() {
+const App = () => {
     return (
         <div>
-            <h1>Hello React</h1>
+            <h1>Hello React 2</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Perspiciatis aliquam autem enim beatae nulla est quod
