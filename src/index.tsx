b26b42332cb1eb20bbb1003a9459a377e6a459c6
Olga <starshova1309@gmail.com>
2022-12-20 12:06:01 +0300

    add title jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
+// no jsx
+// const title = React.createElement('h1', { id: 'title' }, 'Hello react')
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-  </React.StrictMode>
-);
+// with jsx
+const title = <h1 id="title">Yo</h1>
 
-
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<React.StrictMode>{title}</React.StrictMode>)
