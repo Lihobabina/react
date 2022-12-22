9654a010b007cf52e36b4809abdba5a1239b11cf
Olga <starshova1309@gmail.com>
2022-12-22 15:36:45 +0300

    add absolute imports

--- src/Title.tsx
+++
@@ -1,7 +0,0 @@
-type TitleProps = {
-    title: string
-}
-
-export const Title = (props: TitleProps) => {
-    return <h1>Hello {props.title}</h1>
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Title } from './Title'
-type Paragraph = {
-    p1: string
-    p2: string
-}
-
-const Content = (props: Paragraph) => {
-    return (
-        <React.Fragment>
-            <p>{props.p1}</p>
-            <p>{props.p2}</p>
-        </React.Fragment>
-    )
-}
-
-const App = () => {
-    return (
-        <>
-            <Title title="Title Export" />
-            <Content p1="lorem1" p2="lorem2" />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
