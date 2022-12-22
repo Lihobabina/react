e597852a793911a21943b5d5f6e187a93d40b0cf
Olga <starshova1309@gmail.com>
2022-12-22 16:23:45 +0300

    add mui

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,8 +1,10 @@
+import CssBaseline from '@mui/material/CssBaseline'
 import Header from 'container/Header/Header'
 type Props = {}
 const App = (props: Props) => {
     return (
         <>
+            <CssBaseline />
             <Header />
         </>
     )
