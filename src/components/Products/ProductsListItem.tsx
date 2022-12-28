3a64d2be64f0b01f429cc0053c636c2e612d6248
Olga <starshova1309@gmail.com>
2022-12-28 19:28:48 +0300

    add counter & change color

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -17,16 +17,28 @@ type Props = {
 
 type State = {
     count: number
+    color: string
 }
 
 class ProductsListItem extends Component<Props, State> {
     state = {
         count: 1,
+        color: 'green',
     }
 
     onIncrement = () => {
         this.setState((prevState: State) => ({
             count: prevState.count + 1,
+        }))
+    }
+    onDecrement = () => {
+        this.setState((prevState: State) => ({
+            count: prevState.count - 1,
+        }))
+    }
+    changeColor = () => {
+        this.setState((prevState: State) => ({
+            color: prevState.color === 'green' ? 'red' : 'green',
         }))
     }
 
@@ -48,8 +60,16 @@ class ProductsListItem extends Component<Props, State> {
                             <span>Capacity: {capacity}</span> Gb
                         </div>
                         <div className="product-price">{price} $</div>
+                        <div>Color: {this.state.color}</div>
+                        <button onClick={this.changeColor}>Change color</button>
                         <div className="product-quantity">
-                            <Button variant="outlined">-</Button>
+                            <Button
+                                variant="outlined"
+                                onClick={this.onDecrement}
+                                disabled={this.state.count <= 1}
+                            >
+                                -
+                            </Button>
                             <TextField value={this.state.count} size="small" />
                             <Button
                                 variant="outlined"
