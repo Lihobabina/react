03ef24158f80e32d40b0f262e638e315f9e44d28
Olga <starshova1309@gmail.com>
2023-01-10 18:31:03 +0300

    add cart page

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,12 +1,21 @@
 import { Typography } from '@mui/material'
-
+import productsArray, {
+    getProductsObject,
+    ProductProps,
+} from 'utils/productsArray'
 type Props = {
     productsInCart: {
         [id: number]: number
     }
+    productsObject?: {
+        [id: number]: ProductProps
+    }
 }
 
-const CartPage = ({ productsInCart }: Props) => {
+const CartPage = ({
+    productsInCart,
+    productsObject = getProductsObject(productsArray),
+}: Props) => {
     return (
         <div>
             <Typography
@@ -18,6 +27,25 @@ const CartPage = ({ productsInCart }: Props) => {
             >
                 Cart{' '}
             </Typography>
+            <div>
+                {Object.keys(productsInCart).map((productId) => (
+                    <div key={productId}>
+                        {productsObject[parseInt(productId)].title}:
+                        {productsInCart[parseInt(productId)]}
+                    </div>
+                ))}
+            </div>
+            <div>
+                Total:{' '}
+                {Object.keys(productsInCart).reduce(
+                    (total, productId) =>
+                        total +
+                        productsInCart[parseInt(productId)] *
+                            productsObject[parseInt(productId)].price,
+                    0
+                )}
+                $
+            </div>
         </div>
     )
 }
