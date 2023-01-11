46104fa0ccdd9dc6bf848df5f5b9a58fe67e526b
Olga <starshova1309@gmail.com>
2023-01-11 14:46:34 +0300

    add change count in cart wuthout changing total price

--- src/components/CartProductsList/CartProductsListItemExt.tsx
+++ src/components/CartProductsList/CartProductsListItemExt.tsx
@@ -2,6 +2,8 @@ import { Card, CardContent, Grid, Button } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import { ProductProps } from 'utils/productsArray'
 import './CartProductsListItem.scss'
+import { useState } from 'react'
+import './CartProductsListItemExt.scss'
 type Props = {
     productCount: number
     product: ProductProps
@@ -12,6 +14,15 @@ const CartProductsListItemExt = ({
     product,
     removeProductFromCart,
 }: Props) => {
+    const [count, setCount] = useState<number>(productCount)
+    const plus = () => {
+        setCount((prevState: number) => prevState + 1)
+    }
+
+    const minus = () => {
+        setCount((prevState: number) => prevState - 1)
+    }
+
     return (
         <Grid item xs={12} sm={4}>
             <Card>
@@ -21,8 +32,16 @@ const CartProductsListItemExt = ({
                     </div>
                     <div>{product.title}</div>
                     <p>Price for one item: {product.price}</p>
-                    <p>Count: {productCount}</p>
-
+                    <h3>Count</h3>
+                    <div className="counter">
+                        <button onClick={minus} disabled={count <= 1}>
+                            -
+                        </button>
+                        <p>{count}</p>
+                        <button onClick={plus} disabled={count >= 10}>
+                            +
+                        </button>
+                    </div>
                     <Button onClick={() => removeProductFromCart(product.id)}>
                         <DeleteIcon />
                     </Button>
